944442a49a09d1914d1d8742e79a00a25dd19cb6
ShiroSora <qpzm3664289@gmail.com>
2017-05-06 16:25:53 +0800

    change icon,set menu

--- app/src/main/java/com/example/user/usinggit/MainActivity.java
+++ app/src/main/java/com/example/user/usinggit/MainActivity.java
@@ -3,9 +3,20 @@ package com.example.user.usinggit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.example.user.usinggit.R.string.about;
+import static com.example.user.usinggit.R.string.exit;
+import static com.example.user.usinggit.R.string.setting;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    static int SETTING = 123;//Menu Item Id
+    static int ABOUT = 456;//Menu Item Id
+    static int EXIT = 789;//Menu Item Id
+    static int MAIN_GROUP_ITEM = 101;//Menu Item GROUP ID
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +30,31 @@ public class MainActivity extends AppCompatActivity {
           第二個參數代表你是Menu Item Id,
           第三個參數是你放入Item的順序,假如你都設為Menu.NONE代表使用預設
           第四個參數就是你想要放入的標題*/
-        menu.add(Menu.NONE, Menu.FIRST + R.string.setting, Menu.NONE, R.string.setting);
-        menu.add(Menu.NONE, Menu.FIRST + R.string.about, Menu.NONE, R.string.about);
-        menu.add(Menu.NONE, Menu.FIRST + R.string.exit, Menu.NONE, R.string.exit);
+        menu.add(MAIN_GROUP_ITEM, SETTING, Menu.NONE, setting);
+        menu.add(MAIN_GROUP_ITEM, ABOUT, Menu.NONE, about);
+        menu.add(MAIN_GROUP_ITEM, EXIT, Menu.NONE, exit);
         return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {//處理按下Menu Item以後的動作
+        if(item.getGroupId() == MAIN_GROUP_ITEM){
+            if(item.getItemId() == EXIT){//按下 "離開" 按鈕 的動作
+                Toast.makeText(getApplicationContext(),
+                        "按下了離開",
+                        Toast.LENGTH_SHORT).show();
+            }
+            if(item.getItemId() == SETTING){//按下 "設定" 按鈕 的動作
+                Toast.makeText(getApplicationContext(),
+                        "按下了設定",
+                        Toast.LENGTH_SHORT).show();
+            }
+            if(item.getItemId() == ABOUT){//按下 "關於" 按鈕 的動作
+                Toast.makeText(getApplicationContext(),
+                        "按下了關於",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
